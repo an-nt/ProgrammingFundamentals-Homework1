6e3908a8ea23a6a454eb8a6ac0701fe4d0a0ec07
an-nt <thienan175@gmail.com>
2020-08-19 10:36:15 +0700

    BT1

---
+++ BT1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int S = 0;
+            int i = 1;
+            string back;
+
+        ReDubug:
+        ReInput:
+            //Nhap va kiem tra thong so dau vao
+            Console.WriteLine("Nhap N");
+            var check = int.TryParse(Console.ReadLine(), out N);
+            if (!check || N <= 0)
+            {
+                Console.WriteLine("Vui long nhap lai N");
+                goto ReInput;
+            }
+
+            //Tinh toan theo yeu cau de bai
+            while (i <= N)
+            {
+                S += i;
+                i++;
+            }
+            Console.WriteLine($"Ket qua S bang {S}");
+
+            //Thuc hien lai chuong trinh
+            Console.WriteLine("Nhap YES de thuc hien lai chuong trinh)");
+            back = Console.ReadLine();
+            if (back == "yes")
+            {
+                goto ReDubug;
+            }
+        }
+    }
+}
