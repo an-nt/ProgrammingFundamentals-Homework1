079aafcaaff239fd738193dd9550a4af638f386a
an-nt <thienan175@gmail.com>
2020-08-19 11:06:38 +0700

    add homework

---
+++ BT3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BT3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Khai bao tham so
+            int N;
+            double S = 0;
+            double i = 1;
+            string back;
+            
+        ReDebug:
+        ReInput:
+            //Nhap va kiem tra thong so dau vao
+            Console.WriteLine("Nhap N");
+            var check = int.TryParse(Console.ReadLine(), out N);
+            if (!check || N < 1)
+            {
+                Console.WriteLine("Vui long nhap lai N");
+                goto ReInput;
+            }
+
+            //Tinh toan theo yeu cau de bai
+            while (i <= N)
+            {
+                S += 1/i;
+                i++;
+            }
+            Console.WriteLine($"Ket qua S bang {S}");
+
+            //Thuc hien lai chuong trinh
+            Console.WriteLine("Nhap YES de thuc hien lai chuong trinh)");
+            back = Console.ReadLine();
+            if (back == "yes")
+            {
+                goto ReDebug;
+            }
+        }
+    }
+}
