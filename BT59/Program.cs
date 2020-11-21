6e40115920af4736800084cd1860baa36eda399b
an-nt <thienan175@gmail.com>
2020-11-21 10:42:26 +0700

    Kiem tra so doi xung

---
+++ BT59/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BT59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so N");
+            int N = int.Parse(Console.ReadLine());
+            int n = 0;
+            for (int i = N; i > 0; i /= 10)
+            {
+                int ChuSo;
+                ChuSo = i % 10;
+                n = 10 * n + ChuSo;
+            }
+            if (N - n == 0)
+            {
+                Console.WriteLine($"{N} la so doi xung");
+                return;
+            }
+            Console.WriteLine($"{N} KHONG la so doi xung");
+        }
+    }
+}
